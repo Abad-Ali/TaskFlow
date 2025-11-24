75e8efdd53e71499425c8f07da71f32e5af696e6
Abad Ali <abad02597@gmail.com>
2025-11-24 18:58:58 +0530

    Update footer text and link label

--- taskflow-frontend/components/Footer.js
+++ taskflow-frontend/components/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <div>
         <div className='bg-gray-200 pt-[1px] w-full'></div>
-        <div className='bg-black text-gray-400 flex flex-col justify-center items-center p-7'>
+        <div className='bg-black text-gray-400 flex flex-col justify-center items-center p-7 text-sm text-center'>
           <p>© 2025 <span className='text-red-500 font-bold font-serif'>TaskFlow</span>. All rights reserved.</p>
           <p>Your productivity partner.Organize smarter. Work better</p>
-          <p>Privacy Policy | Terms of Service | <a href='https://github.com/Abad-Ali' target='_blank'>Contact Us</a></p>
+          <p>Privacy Policy | Terms of Service | <a href='https://github.com/Abad-Ali' target='_blank'>Contact</a></p>
         </div>
     </div>
   )
